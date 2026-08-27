923e2c228f5fae8543d1c429c56698f7cbb2e764
Arthur_HM <164037758+ArthurMenegat@users.noreply.github.com>
2026-08-26 22:27:59 -0300

    Alteração data_loader e glossário atualizado

--- Codes/data_loader.py
+++ Codes/data_loader.py
@@ -28,3 +28,38 @@ class GPTDatasetV1(Dataset):
 
     def __getitem__(self, idx):
         return self.input_ids[idx], self.target_ids[idx]
+
+def create_dataloader(txt, batch_size=4, max_length=256, stride=128,
+                          shuffle=True, drop_last=True, num_workers=0):
+    """
+    Recebe o texto bruto e devolve um DataLoader
+    (do PyTorch) já pronto, que entrega os dados em lotes.
+    batch_size -> quantas sequências vêm em cada lote
+    max_length -> quantos tokens por sequência
+    stride     -> de quanto em quanto a janela "anda" sobre o texto
+    """
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
+
+if __name__ == "__main__":
+    with open("the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read()
+
+    dataloader = create_dataloader(raw_text, batch_size=1, max_length=4, stride=4, shuffle=False)
+
+    data_iter = iter(dataloader)
+    inputs, targets = next(data_iter)
+
+    print("Entradas:\n", inputs)
+    print("Alvos:\n", targets)
